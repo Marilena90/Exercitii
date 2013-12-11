31786d54ca574965dcf327d43ed20cafa61304c9
Marilena90 <miron.marilena@ymail.com>
2013-12-11 23:18:28 +0200

    Aplicatia 16

---
+++ 10.12.2013/Aplicatia16/CostAnualCurent.dpr
@@ -0,0 +1,24 @@
+program CostAnualCurent;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+const
+  cOreAn= 8760;
+
+var
+  vCostKWh, vNrKWh, vBecAn: Double;
+
+begin
+  Writeln;
+  Write('Introdu costu unui kilowat-ora (KWh): ');
+  Readln(vCostKWh);
+  write('Introdu numarul de kilowati-ora (KWh): ');
+  Readln(vNrKWh);
+  vBecAn:= (vCostKWh * vNrKWh * cOreAn);
+  Writeln('Costul anual al consumului de curent al unui bec este: ' + FloatToStr(vBecAn) + ' lei ');
+  Readln;
+end.
+ 
