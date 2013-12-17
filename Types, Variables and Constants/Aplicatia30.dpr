5edfdbf1409cd91c7b80cebbd8948f02d3a0e3e6
Marilena90 <miron.marilena@ymail.com>
2013-12-17 21:47:05 +0200

    Aplicatii

---
+++ Types, Variables and Constants/Aplicatia30.dpr
@@ -0,0 +1,24 @@
+program Aplicatia30;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+var
+  vZi, vLuna, vAn, vZiSaptamana: Integer;
+  vDataNasterii: TDateTime;
+
+begin
+  Writeln;
+  Write('Introdu ziua nasterii: ');
+  Readln(vZi);
+  Write('Introdu luna nasterii: ');
+  Readln(vLuna);
+  Write('Introdu anul nasterii: ');
+  Readln(vAn);
+  vDataNasterii:= EncodeDate(vAn, vLuna, vZi);
+  vZiSaptamana:= DayOfWeek(vDataNasterii)-1;
+  Writeln('Ziua saptamanii este: ' + IntToStr(vZiSaptamana));
+  Readln;
+end.
