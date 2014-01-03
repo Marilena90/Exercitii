70d2465b8ab2c5993f82b4ae572c6f011415b7e7
Marilena90 <miron.marilena@ymail.com>
2014-01-03 20:50:51 +0200

    Aplicatii

--- 02. Conditionals, Loops/Aplicatia42.dpr
+++ 02. Conditionals, Loops/Aplicatia42.dpr
@@ -5,22 +5,39 @@ program Aplicatia42;
 uses
   SysUtils;
 
+const
+  cSir: set of Char = ['A'..'Z'];
+
 var
-  s: string;
-  i, j, k: integer;
+  sir: string;
+  i, vVocalaA, vVocalaE, vVocalaI, vVocalaO, vVocalaU, vConsoane: integer;
 
 begin
   Write('Introduceti o propozitie:');
-  Readln(s);
-  k := 0;
-  for i := 1 to length(s) do
-    if (s[i] = 'a') or (s[i] = 'e') or (s[i] = 'i') or (s[i] = 'o') or (s[i] = 'u')
-      then k := k + 1;
-  Writeln('Propozitia introdusa contine ', k, ' vocale');
-  Readln;
-  if (s[i] <> 'a') or (s[i] <> 'e') or (s[i] <> 'i') or (s[i] <> 'o') or (s[i] <> 'u')
-    then k := k + 1;
-  Writeln('Propozitia introdusa contine ', k, ' consoane');
+  Readln(sir);
+  vVocalaA := 0; vVocalaE := 0; vVocalaI := 0; vVocalaO := 0; vVocalaU := 0;
+  vConsoane := 0;
+  for i := 1 to Length(sir) do
+  begin
+    if UpCase(sir[i]) in cSir then
+    begin
+      case UpCase(sir[i]) of
+        'A': vVocalaA := vVocalaA + 1;
+        'E': vVocalaE := vVocalaE + 1;
+        'I': vVocalaI := vVocalaI + 1;
+        'O': vVocalaO := vVocalaO + 1;
+        'U': vVocalaU := vVocalaU + 1;
+      else if not (sir[i] in ['A', 'E', 'I', 'O', 'U']) then
+        vConsoane := vConsoane + 1;
+      end;
+    end;
+  end;
+  Writeln('Propozitia introdusa contine ', vVocalaA, ' vocale a, ' + FloatToStr((vVocalaA / vConsoane) * 100)); //in loc de vConsoane pune sir!!!
+  Writeln('Propozitia introdusa contine ', vVocalaE, ' vocale e, ' + FloatToStr((vVocalaE / vConsoane) * 100));
+  Writeln('Propozitia introdusa contine ', vVocalaI, ' vocale i, ' + FloatToStr((vVocalaI / vConsoane) * 100));
+  Writeln('Propozitia introdusa contine ', vVocalaO, ' vocale o, ' + FloatToStr((vVocalaO / vConsoane) * 100));
+  Writeln('Propozitia introdusa contine ', vVocalaU, ' vocale u, ' + FloatToStr((vVocalaU / vConsoane) * 100));
+  Writeln('Propozitia introdusa contine ', vConsoane, ' consoane');
   Readln;
 end.
 
